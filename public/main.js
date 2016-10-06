ca84265959893d96045ae97c3f302c2f20716c6c
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-06 11:27:05 -0500

    created basic server and main.js file for testing DOM interaction

--- public/main.js
+++ public/main.js
@@ -1,1 +1,5 @@
 'use strict'
+
+const test = document.querySelector('.test');
+
+test.innerTest = "It's a test";
--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-'use strict'
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+
