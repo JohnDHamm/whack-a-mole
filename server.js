21dc6a9b3da8c0726be0096f2bc13f11245bdebe
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-07 10:29:57 -0500

    created connection to mongo DB, created model with defaults, and returning values from DB

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/whack-a-mole'
+
 
 app.set('view engine', 'pug');
 
@@ -10,5 +14,27 @@ app.get('/', (req, res) => res.render('index'));
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+// set up mongoDB
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () => {
+  app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
+})
 
+// set up game model for DB
+const Game = mongoose.model('game', {
+  board: {
+    type: [
+      [String, String, String],
+      [String, String, String],
+    ],
+    default: [
+      ['', '', 'X'],
+      ['', '', ''],
+    ]
+  }
+})
+
+// creates a new Game object in the mongo DB with the default value from the Game model
+// the whole document that was just created is returned from the DB from the create function
+// and can be used after the then statement
+Game.create({}).then(game => console.log("game", game));
